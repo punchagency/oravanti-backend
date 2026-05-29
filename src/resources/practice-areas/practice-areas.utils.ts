8923ba82b7648c15ea53e81cebfd21745bd59b94
Ubonisrael Akpanudoh <ubonisraelakpanudoh@punch.agency>
2026-05-29 10:34:38 +0100

    Require practice area subscriptions for cases

--- src/resources/practice-areas/practice-areas.utils.ts
+++ src/resources/practice-areas/practice-areas.utils.ts
@@ -1,12 +1,17 @@
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { db } from "../../db/client";
+import { firmPracticeAreas } from "../../db/schema/firm-practice-areas";
 import { practiceAreas } from "../../db/schema/practice-areas";
+import {
+  SubscriptionStatus,
+  subscriptions,
+} from "../../db/schema/subscriptions";
 import { BadRequestError, NotFoundError } from "../../utils/error/app-error";
 
 export const normalizePracticeAreaName = (name: string) => name.trim();
 
 export const ensurePracticeAreaExists = async (
-  _firmId: string,
+  firmId: string,
   practiceAreaId?: string,
 ) => {
   if (!practiceAreaId) {
@@ -20,6 +25,23 @@ export const ensurePracticeAreaExists = async (
 
   if (!practiceArea) {
     throw new NotFoundError("Practice area not found");
+  }
+
+  const [subscription] = await db
+    .select({ id: subscriptions.id })
+    .from(firmPracticeAreas)
+    .innerJoin(subscriptions, eq(subscriptions.id, firmPracticeAreas.subscriptionId))
+    .where(
+      and(
+        eq(firmPracticeAreas.firmId, firmId),
+        eq(firmPracticeAreas.practiceAreaId, practiceAreaId),
+        eq(firmPracticeAreas.active, true),
+        eq(subscriptions.status, SubscriptionStatus.ACTIVE),
+      ),
+    );
+
+  if (!subscription) {
+    throw new BadRequestError("Firm is not subscribed to this practice area");
   }
 
   return practiceArea;
