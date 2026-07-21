16cba50fabab426cfac79d81c9e5d5f27f99b75f
samuel adekoya <samueladexx@gmail.com>
2026-07-21 14:35:09 +0100

    remove workflow event type migration

--- migrations/add-workflow-event-types.sql
+++
@@ -1,8 +0,0 @@
--- Add workflow event types to the case_event_type enum
-ALTER TYPE case_event_type ADD VALUE IF NOT EXISTS 'workflow_initialized';
-ALTER TYPE case_event_type ADD VALUE IF NOT EXISTS 'module_activated';
-ALTER TYPE case_event_type ADD VALUE IF NOT EXISTS 'step_assigned';
-ALTER TYPE case_event_type ADD VALUE IF NOT EXISTS 'step_completed';
-ALTER TYPE case_event_type ADD VALUE IF NOT EXISTS 'step_submitted_for_review';
-ALTER TYPE case_event_type ADD VALUE IF NOT EXISTS 'step_approved';
-ALTER TYPE case_event_type ADD VALUE IF NOT EXISTS 'step_rejected';
